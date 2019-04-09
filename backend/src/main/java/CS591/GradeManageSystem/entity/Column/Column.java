1256255b07beb6fde8f93e3b42d7ec47c2cfffe3
Yuhang Miao <miaoyh@bu.edu>
2019-04-09 16:10:51 -0400

    implement the designed  entites

---
+++ backend/src/main/java/CS591/GradeManageSystem/entity/Column/Column.java
@@ -0,0 +1,10 @@
+package CS591.GradeManageSystem.entity.Column;
+
+public abstract class Column {
+
+    public abstract String getColumnName();
+
+    public abstract void setColumnName(String name);
+
+    public abstract String getColumnId();
+}
